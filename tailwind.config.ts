cd5b183c12fcd2dd36f7537fe35c9987edbeb5eb
Randa Firman Putra <randafrmn@gmail.com>
2024-10-15 16:13:51 +0700

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,24 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        neutral: {
+          light: {
+            veryLightGray: 'hsl(0, 0%, 98%)',
+            veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+            lightGrayishBlue: 'hsl(233, 11%, 84%)',
+            darkGrayishBlue: 'hsl(236, 9%, 61%)',
+            veryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+          },
+          dark: {
+            veryDarkBlue: 'hsl(235, 21%, 11%)',
+            veryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+            lightGrayishBlue: 'hsl(234, 39%, 85%)',
+            lightGrayishBlueHover: 'hsl(236, 33%, 92%)',
+            darkGrayishBlue: 'hsl(234, 11%, 52%)',
+            veryDarkGrayishBlue1: 'hsl(233, 14%, 35%)',
+            veryDarkGrayishBlue2: 'hsl(237, 14%, 26%)',
+          }
+        },
       },
     },
   },
