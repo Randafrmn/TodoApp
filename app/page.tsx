72947b1b05136740f9fa6e2ca3c388cefb27517a
Randa Firman Putra <randafrmn@gmail.com>
2024-10-15 16:30:21 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
+/** @format */
 
 export default function Home() {
-  const images = {
-    desktopLight: "/images/bg-desktop-light.jpg",
-    desktopDark: "/images/bg-desktop-dark.jpg",
-  };
+	const images = {
+		desktopLight: "/images/bg-desktop-light.jpg",
+		desktopDark: "/images/bg-desktop-dark.jpg",
+	};
 
-  return (
-    <div className="w-screen h-64 bg-gradient-to-r from-[hsl(192,100%,67%)] to-[hsl(280,87%,65%)]"
-        style={{ 
-          backgroundImage: `url(${images.desktopLight})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat'
-         }}
-    >
-    
-    </div>
-  )
+	return (
+		<div
+			className="w-screen h-64 bg-gradient-to-r from-[hsl(192,100%,67%)] to-[hsl(280,87%,65%)]"
+			style={{
+				backgroundImage: `url(${images.desktopLight})`,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				backgroundRepeat: "no-repeat",
+			}}
+		></div>
+	);
 }
